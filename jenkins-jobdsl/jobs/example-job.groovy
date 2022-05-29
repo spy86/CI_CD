b76501fdd21c08d873d7bf45bd5292a4354e5901
spy86 <contact@mmichalski.com>
2022-05-29 13:58:08 +0200

    update

--- jenkins-jobdsl/jobs/example-job.groovy
+++ jenkins-jobdsl/jobs/example-job.groovy
@@ -1,5 +1,12 @@
-job('example') {
-  steps {
-    shell('echo Hello World!')
-  }
+pipelineJob('job-dsl-artifactory-pipeline-example') {
+    parameters {
+        stringParam('SERVER_ID', SERVER_ID, 'Enter Artifactory server ID')
+    }
+
+    definition {
+        cps {
+            script(readFileFromWorkspace('jenkins-examples/pipeline-examples/scripted-examples/declarative-example/Jenkinsfile'))
+            sandbox()
+        }
+    }
 }
