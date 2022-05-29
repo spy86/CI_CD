bb9be53b35866105bfdc24a4e66b501723a014e3
spy86 <contact@mmichalski.com>
2022-05-29 14:09:50 +0200

    update view

--- jenkins-jobdsl/views/example_view.groovy
+++ jenkins-jobdsl/views/example_view.groovy
@@ -1,5 +1,23 @@
-listView('example') {
+listView('project-A') {
+    description('All jobs')
+    filterBuildQueue()
+    filterExecutors()
     jobs {
         name('example')
+        regex(/example.+/)
+    }
+    jobFilters {
+        status {
+            status(Status.SUCESS)
+        }
+    }
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
     }
 }
